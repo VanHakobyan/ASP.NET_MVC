3d7b799c7644d7734b994a6a036b85002896b84f
VanHakobyan <van19962013@mail.ru>
2017-03-18 00:50:51 +0400

    remove

---
+++ FirstWebAPP/FirstMVC/Controllers/CurrentController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstMVC.Controllers
+{
+    public class CurrentController : Controller
+    {
+        // GET: Current
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
