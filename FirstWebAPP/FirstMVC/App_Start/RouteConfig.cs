eab4b96d9b528292e04b1f594eb6ff255d95f713
VanHakobyan <van19962013@mail.ru>
2017-03-24 18:30:31 +0400

    add MapRoute

--- FirstWebAPP/FirstMVC/App_Start/RouteConfig.cs
+++ FirstWebAPP/FirstMVC/App_Start/RouteConfig.cs
@@ -16,8 +16,14 @@ namespace FirstMVC
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index",  id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+            routes.MapRoute(
+                name: "Van",
+                url: "{start}/{stop}",
+                defaults: new { start = "Home", stop = "Index" }
+            );
+
         }
     }
 }
